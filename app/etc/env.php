98665726161a97d29eec419d7158a7e023cae33b
Pavel <pavlikzelenyj@gmail.com>
2023-05-17 13:08:17 +0000

    [main] First commit

---
+++ app/etc/env.php
@@ -0,0 +1,56 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => '9af38453afda96c7862c7d10eb493ba4'
+    ],
+    'session' => [
+        'save' => 'files'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1',
+                'dbname' => 'testform',
+                'username' => 'root',
+                'password' => '',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'full_page' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'translate' => 1,
+        'config_webservice' => 1,
+        'compiled_config' => 1
+    ],
+    'install' => [
+        'date' => 'Tue, 16 May 2017 09:13:48 +0000'
+    ],
+    'downloadable_domains' => [
+        'localhost'
+    ]
+];
